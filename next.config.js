3ba5a95cd121d1706e751a6e17fed1c8cb2d06ff
chirayumit001 <chirayumit001@gmail.com>
2021-10-15 22:50:45 +0530

    Set up webpack

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 module.exports = {
-  reactStrictMode: true,
-  eslint: {
-    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-    // ignoreDuringBuilds: true,
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+	reactStrictMode: true,
+	eslint: {
+		dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+		// ignoreDuringBuilds: true,
+	},
+
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
 }
